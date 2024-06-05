eb18fe5086fa3612579da107d9af41809781ffaa
Berk Elmas <berkelmas96@gmail.com>
2024-06-05 03:34:41 +0300

    add needed models and custom nodes

--- build/COPY_ROOT_EXTRA/opt/ai-dock/bin/build/layer1/init.sh
+++ build/COPY_ROOT_EXTRA/opt/ai-dock/bin/build/layer1/init.sh
@@ -9,10 +9,35 @@ PYTHON_PACKAGES=(
 
 NODES=(
     "https://github.com/ltdrdata/ComfyUI-Manager"
+    ## TILE UPSCALER
+    "https://github.com/ssitu/ComfyUI_UltimateSDUpscale"
+    ## LAMA OBJECT REMOVAL
+    "https://github.com/sipherxyz/comfyui-art-venture"
+    ## SDXL Prompt Styler
+    "https://github.com/twri/sdxl_prompt_styler"
+    ## REMBG (Image background remover)
+    "https://github.com/Jcd1230/rembg-comfyui-node.git"
+    ## IP Adapter Plus Custom Node
+    "https://github.com/cubiq/ComfyUI_IPAdapter_plus"
+    ## TILE UPSCALER
+    "https://github.com/ssitu/ComfyUI_UltimateSDUpscale"
+    ## BASE64 Image loader
+    "https://github.com/Acly/comfyui-tooling-nodes"
+    ## MIXLAB
+    "https://github.com/shadowcz007/comfyui-mixlab-nodes"
+    ## WAS NODE ???
+    "https://github.com/WASasquatch/was-node-suite-comfyui"
+    ## CONTROLNET
+    "https://github.com/Fannovel16/comfyui_controlnet_aux"
+    ## IMPACT PACK
+    "https://github.com/ltdrdata/ComfyUI-Impact-Pack"
 )
 
 CHECKPOINT_MODELS=(
-    #"https://huggingface.co/runwayml/stable-diffusion-v1-5/resolve/main/v1-5-pruned-emaonly.ckpt"
+    ## JUGGERNAUT SDXL
+    "https://civitai.com/api/download/models/471120 Juggernaut_X_RunDiffusion_Hyper.safetensors"
+    ## SD 1.5
+    "https://huggingface.co/runwayml/stable-diffusion-v1-5/resolve/main/v1-5-pruned-emaonly.ckpt v1-5-pruned-emaonly.ckpt"
     #"https://huggingface.co/stabilityai/stable-diffusion-2-1/resolve/main/v2-1_768-ema-pruned.ckpt"
     #"https://huggingface.co/stabilityai/stable-diffusion-xl-base-1.0/resolve/main/sd_xl_base_1.0.safetensors"
     #"https://huggingface.co/stabilityai/stable-diffusion-xl-refiner-1.0/resolve/main/sd_xl_refiner_1.0.safetensors"
@@ -35,6 +60,10 @@ ESRGAN_MODELS=(
 )
 
 CONTROLNET_MODELS=(
+    ## TILE UPSCALER CONTROLNET
+    "https://huggingface.co/lllyasviel/ControlNet-v1-1/resolve/main/control_v11f1e_sd15_tile.pth control_v11f1e_sd15_tile.pth"
+    ## DEPTH CONTROLNET
+    "https://huggingface.co/stabilityai/control-lora/resolve/main/control-LoRAs-rank256/control-lora-depth-rank256.safetensors control-lora-depth-rank256.safetensors"
     #"https://huggingface.co/webui/ControlNet-modules-safetensors/resolve/main/control_canny-fp16.safetensors"
     #"https://huggingface.co/webui/ControlNet-modules-safetensors/resolve/main/control_depth-fp16.safetensors"
     #"https://huggingface.co/kohya-ss/ControlNet-diff-modules/resolve/main/diff_control_sd15_depth_fp16.safetensors"
@@ -52,6 +81,21 @@ CONTROLNET_MODELS=(
     #"https://huggingface.co/webui/ControlNet-modules-safetensors/resolve/main/t2iadapter_seg-fp16.safetensors"
     #"https://huggingface.co/webui/ControlNet-modules-safetensors/resolve/main/t2iadapter_sketch-fp16.safetensors"
     #"https://huggingface.co/webui/ControlNet-modules-safetensors/resolve/main/t2iadapter_style-fp16.safetensors"
+)
+
+UPSCALE_MODELS=(
+    ## 4x Ultrasharp Upscaler
+    "https://civitai.com/api/download/models/125843 4xUltrasharp_4xUltrasharpV10.pt"
+)
+
+CLIP_VISION=(
+    ## IP ADAPTER CLIP VISION
+    "https://huggingface.co/h94/IP-Adapter/resolve/main/models/image_encoder/model.safetensors clip-vision-1.safetensors"
+)
+
+IP_ADAPTER=(
+    ## IP ADAPTER
+    "https://huggingface.co/h94/IP-Adapter/resolve/main/sdxl_models/ip-adapter-plus_sdxl_vit-h.safetensors ip-adapter-plus_sdxl_vit-h.safetensors"
 )
 
 ### DO NOT EDIT BELOW HERE UNLESS YOU KNOW WHAT YOU ARE DOING ###
@@ -74,7 +118,16 @@ function build_extra_start() {
     build_extra_get_models \
         "/opt/storage/stable_diffusion/models/esrgan" \
         "${ESRGAN_MODELS[@]}"
-     
+    build_extra_get_models \
+        "/opt/storage/stable_diffusion/models/upscale_models" \
+        "${UPSCALE_MODELS[@]}"
+    build_extra_get_models \
+        "/opt/storage/stable_diffusion/models/clip_vision" \
+        "${CLIP_VISION[@]}"
+    build_extra_get_models \
+        "/opt/storage/stable_diffusion/models/ipadapter" \
+        "${IP_ADAPTER[@]}"
+
     cd /opt/ComfyUI && \
     micromamba run -n comfyui -e LD_PRELOAD=libtcmalloc.so python main.py \
         --cpu \
