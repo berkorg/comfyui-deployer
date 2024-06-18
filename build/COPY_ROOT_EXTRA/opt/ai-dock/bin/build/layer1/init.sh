1453ae027224bac15b3e6dfcf3e59ae8ad8d2cee
Berk Elmas <berkelmas96@gmail.com>
2024-06-18 16:33:06 +0300

    rembg onxx file download removed

--- build/COPY_ROOT_EXTRA/opt/ai-dock/bin/build/layer1/init.sh
+++ build/COPY_ROOT_EXTRA/opt/ai-dock/bin/build/layer1/init.sh
@@ -118,9 +118,9 @@ function build_extra_start() {
     # build_extra_get_models \
     #     "/opt/storage/stable_diffusion/models/esrgan" \
     #     "${UPSCALE_MODELS[@]}"
-    build_extra_get_models \
-        "/opt/storage/stable_diffusion/models/rembg" \
-        "${REMBG[@]}"
+    # build_extra_get_models \
+    #     "/opt/storage/stable_diffusion/models/rembg" \
+    #     "${REMBG[@]}"
     # build_extra_get_models \
     #     "/opt/storage/stable_diffusion/models/clip_vision" \
     #     "${CLIP_VISION[@]}"
